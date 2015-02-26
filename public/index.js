fa6ffd2e1e19d158700b0168866046ad2ff8e05d
AaronKolenda <kolenda.a@gmail.com>
2015-02-26 15:22:51 -0500

    Making basic UI

--- public/index.js
+++ public/index.js
@@ -1,1 +1,34 @@
 var x = function(){}
+
+var listTasks = function(callback) {
+
+  $.ajax({
+    url: "/tasks",
+    method: "GET",
+    success: function(data) {
+      callback(data)
+    }
+  })
+
+}
+
+var displayTasks = function(data) {
+	console.log(data);
+}
+
+listTasks(displayTasks);
+
+$(document).ready(function(){
+
+	$("#searchInput").click(function(){
+		$("#searchInput").val(" ");
+		$("#searchInput").css("color", "#555555;");
+	});
+
+	$("#createInput").click(function(){
+		$("#createInput").val(" ");
+		$("#createInput").css("color", "#555555;");
+	});
+
+});
+
