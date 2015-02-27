d3dd54a87efe99f9c16e947bd48ba2a78cd19a53
AaronKolenda <kolenda.a@gmail.com>
2015-02-27 15:09:31 -0500

    adding toggling complete/incomplete functionality

--- public/index.js
+++ public/index.js
@@ -5,6 +5,9 @@ var getTemplates = function(){
   var tasksString = $("#tasks-template").text()
   templates.tasksInfo = Handlebars.compile(tasksString);
 
+  var singleString = $("#single-task-template").text()
+  templates.singleInfo = Handlebars.compile(singleString);
+
   };
 
 var listTasks = function(callback) {
@@ -43,12 +46,79 @@ var listIncompleteTasks = function(callback) {
 
 }
 
+var getTaskByID = function(id, callback) {
+
+	var urlString = ("/tasks/" + id);
+
+  $.ajax({
+    url: urlString,
+    method: "GET",
+    success: function(data) {
+      callback(id, data)
+    }
+  })
+
+}
+
+var displayOneTask = function(id, data) {
+
+	var markAsComplete = "Mark as Complete";
+	var markAsIncomplete = "Mark as Incomplete";
+
+		if (data.complete === true) {
+			data.button = markAsIncomplete;
+		}
+
+		if (data.complete === false) {
+			data.button = markAsComplete;
+		}
+
+	
+	var singleListString = templates.singleInfo(data);
+	$("#" + id).html(singleListString);
+
+
+}
+
+var markComplete = function(id, value) {
+
+	console.log("in markComplete value is " + value);
+
+	var urlStringClose = ("/tasks/" + id + "/close");
+	var urlStringOpen = ("/tasks/" + id + "/reopen");
+
+	if (value === "false") {
+		  $.ajax({
+		    url: urlStringClose,
+		    method: "POST",
+		    success: function(data) {
+		      console.log(data);
+		      //callback(data);
+		    }
+		  })
+	}
+
+	if (value === "true") {
+		  $.ajax({
+		    url: urlStringOpen,
+		    method: "POST",
+		    success: function(data) {
+		      console.log(data);
+		      //callback(data);
+		    }
+		  })
+	}
+
+	getTaskByID(id, displayOneTask);
+
+}
+
 var displayTasks = function(data) {
 
 	var markAsComplete = "Mark as Complete";
 	var markAsIncomplete = "Mark as Incomplete";
 
-	_.each(data, function(element, index){
+	_.each(data, function(element, index) {
 
 		if (element.complete === true) {
 			element.button = markAsIncomplete;
@@ -57,7 +127,6 @@ var displayTasks = function(data) {
 		if (element.complete === false) {
 			element.button = markAsComplete;
 		}
-		console.log(element);
 
 	});
 
@@ -91,6 +160,14 @@ $(document).ready(function(){
 	$("#incomplete").click(function(){
 		listIncompleteTasks(displayTasks);
 	});
+
+	$(document).on('click', ".mark", function(){
+            var buttonID = this.id;
+            var buttonValue = this.value;
+            markComplete(buttonID, buttonValue);
+        });
+
+
 
 });
 
